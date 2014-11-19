9134fd478bb90d8baa1de389d7f6e47d09f58a42
roryg11 <Rory.C.Grant@gmail.com>
2014-11-19 13:48:33 -0800

    quotes at bottom of home page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,9 @@
 class WelcomeController < ApplicationController
   def home
+    @quotes = {
+      "Arnold Schwarzenegger" => "Failure is not an option. Everyone has to succeed",
+      "Steve Jobs" => "Your time is limited, so don't waste it living someone else's life.",
+      "Papa John" => "Better ingredients. Better Pizza",
+    }
   end
 end
