e0f1ea1a4d970c43931de9314cde0d12c1bf9b5e
roryg11 <Rory.C.Grant@gmail.com>
2014-12-05 13:22:12 -0800

    adding an index page with a new button

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141202230708) do
+ActiveRecord::Schema.define(version: 20141205211611) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "projects", force: true do |t|
+    t.string "name", null: false
+  end
 
   create_table "tasks", force: true do |t|
     t.string   "description"
