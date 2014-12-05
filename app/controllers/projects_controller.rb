a74d34cd161d96e18fb477bf1137d5db1fb39402
roryg11 <Rory.C.Grant@gmail.com>
2014-12-05 14:05:12 -0800

    projects have a show page with a flash message for successful creation and edit and delete button, which redirects to index page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,4 +4,36 @@ class ProjectsController < ApplicationController
     @projects = Project.all
   end
 
+  def new
+    @project=Project.new
+  end
+
+  def create
+    @project=Project.new(project_params)
+    if @project.save
+      flash[:notice] = "Project was successfully created"
+      redirect_to project_path(@project)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @project=Project.find(params[:id])
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    redirect_to projects_path
+    flash[:notice] = "Project successfully deleted"
+  end
+
+  private
+  def project_params
+    params.require(:project).permit(
+      :name
+    )
+  end
+
 end
