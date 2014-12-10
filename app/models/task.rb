a9429b888f0b9a64e411410c98084884ad0ed1be
roryg11 <Rory.C.Grant@gmail.com>
2014-12-09 18:46:50 -0800

    user cannot create task due date in the past

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,11 @@
 class Task < ActiveRecord::Base
   validates :description, presence: true
-  
+  validate :due_date_cannot_be_in_the_past
+
+  def due_date_cannot_be_in_the_past
+    if due_date.present? && due_date < Date.today
+      errors.add(:due_date, "due date cannot be in the past")
+    end
+  end
+
 end
