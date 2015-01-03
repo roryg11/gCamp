6636fa8e06ac46b3ff01aa09aaf39caaa8e7bdb0
roryg11 <Rory.C.Grant@gmail.com>
2015-01-03 15:44:05 -0700

    making sure that members can still be added to other projects

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -7,7 +7,7 @@ class Membership < ActiveRecord::Base
   belongs_to :user
 
   def project_user_id_does_not_exist?
-    if project.memberships.where(user_id: user_id)
+    if Membership.where(project_id: project_id, user_id: user_id).length > 0 && !self.id
       errors.add(:base, "member already exists")
     end
   end
