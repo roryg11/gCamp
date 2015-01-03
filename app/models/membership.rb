9d6c7876cc46476475063fee97ce0320d7e054d1
roryg11 <Rory.C.Grant@gmail.com>
2015-01-03 15:29:16 -0700

    membership validation to keep members from being duplicated in a project

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,6 +1,15 @@
 class Membership < ActiveRecord::Base
-  validates :user_id, :role, presence: true
+  validates :role, presence: true
+  validates :user_id, presence: true
+  validate :project_user_id_does_not_exist?
 
   belongs_to :project
   belongs_to :user
+
+  def project_user_id_does_not_exist?
+    if project.memberships.where(user_id: user_id)
+      errors.add(:base, "member already exists")
+    end
+  end
+
 end
